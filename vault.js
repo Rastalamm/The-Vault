7c1f5856677d7b2c0807718269d9ad985a17ccdc
Judah Lamm <Judah.Lamm@gmail.com>
2015-05-30 15:00:28 -1000

    completion of the vault exercise

--- vault.js
+++ vault.js
@@ -1,4 +1,19 @@
 'use strict';
 module.exports = function() {
-  
+
+   var vault = {
+
+    setValue : function(key, value){
+      this.key = value;
+    },
+
+    getValue : function(key){
+      if(this.key){
+        return vault.key;
+      }else{
+        return null;
+      }
+    }
+  };
+  return vault;
 };
